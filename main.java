0c7a63b04f2ce31404782aefbf044b79242d997d
Muhammad Abdullah <ma3472995@gmail.com>
2024-11-15 19:46:11 +0500

    Fix critical bug in Main class

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the application!");
-// greetUser(); // Removed this line to introduce a bug
+        greetUser(); // Removed this line to introduce a bug
     }
 
     public static void greetUser() {
