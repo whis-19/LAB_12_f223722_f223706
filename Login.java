a9aa668495a15e15bd265ef67baba9558440b58c
Muhammad Abdullah <ma3472995@gmail.com>
2024-11-15 18:57:44 +0500

    Experiment 1

--- Login.java
+++ Login.java
@@ -1,5 +1,7 @@
 public class Login {
-public boolean authenticate(String username, String password) {
-return username.equals("admin") && password.equals("password123");
-}
+    public boolean authenticate(String username, String password) {
+        System.out.println("Authenticating user...");
+        return username.equals("admin") && password.equals("password123");
+        }
+
 }
