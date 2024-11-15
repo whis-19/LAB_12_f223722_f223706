d63cd122d2eb529cdc8059847d0abf8e5178ff4d
Muhammad Abdullah <ma3472995@gmail.com>
2024-11-15 18:36:02 +0500

    Initialized

---
+++ main.java
@@ -0,0 +1,10 @@
+public class Main {
+public static void main(String[] args) {
+    System.out.println("Welcome to the application!");
+    greetUser();
+}
+
+public static void greetUser() {
+    System.out.println("Hello, User!");
+}
+}
