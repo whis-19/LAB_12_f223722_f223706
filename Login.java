82bc81ae56323791d21f9f7efa7b31cebeba2c3c
Muhammad Abdullah <ma3472995@gmail.com>
2024-11-15 18:58:51 +0500

    Experiment 2

--- Login.java
+++ Login.java
@@ -1,5 +1,9 @@
 public class Login {
     public boolean authenticate(String username, String password) {
+        if (username == null || password == null) {
+        System.out.println("Invalid input");
+        return false;
+        }
         System.out.println("Authenticating user...");
         return username.equals("admin") && password.equals("password123");
         }
